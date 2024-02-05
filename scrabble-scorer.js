50157ee53e07e5f603fb322b555accc780059a23
ashleyodberg <115903271+ashleyodberg@users.noreply.github.com>
2024-02-05 00:36:29 -0800

    very slowly working through graded assignment 2: scrabble scorer

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -12,6 +12,7 @@ const oldPointStructure = {
   10: ['Q', 'Z']
 };
 
+
 function oldScrabbleScorer(word) {
 	word = word.toUpperCase();
 	let letterPoints = "";
@@ -23,7 +24,6 @@ function oldScrabbleScorer(word) {
 		 if (oldPointStructure[pointValue].includes(word[i])) {
 			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
 		 }
- 
 	  }
 	}
 	return letterPoints;
@@ -32,7 +32,7 @@ function oldScrabbleScorer(word) {
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
-function initialPrompt() {
+function initialPrompt() { //prompts the user to enter a word for scoring
    console.log("Let's play some scrabble! Enter a word:");
 };
 
@@ -50,9 +50,11 @@ function transform() {};
 
 let newPointStructure;
 
-function runProgram() {
+function runProgram() { //runs the initial prompt when the program starts
    initialPrompt();
-   
+   const userWord = input.question("Enter a word: ");
+   const score = oldScrabbleScorer(userWord);
+   console.log(score);
 }
 
 // Don't write any code below this line //
